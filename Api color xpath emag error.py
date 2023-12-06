92c6265fcc2fbf6eb48f6f4b67dc279a65c28757
Alexander D <114608225+vodav0da@users.noreply.github.com>
2023-12-06 15:28:10 +0200

    new price tracker smooth.py
    I scrapped the entire code and rewrote it to be much smoother and faster. No longer has errors(unless the websites themselves are down, although I don't have control over that) and as a final touch i remade the GUI using the PyQt library for a more modern look. In the future I will add more websites for access and a feature that provides links for all of the products at the end of the program

--- Api color xpath emag error.py
+++ Api color xpath emag error.py
@@ -1,222 +1,135 @@
-import tkinter as tk
-from tkinter import font
-from selenium.common.exceptions import NoSuchElementException
+from PyQt5.QtWidgets import QApplication, QDialog, QLineEdit, QVBoxLayout, QPushButton, QLabel
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore
 
 
-class GUI:
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.title("Price tracker")
-        self.root.geometry("620x290")
-        self.root.configure(bg="#2E2E2E")  # Set background color
+class ProductInputDialog(QDialog):
+    def __init__(self, parent=None):
+        super(ProductInputDialog, self).__init__(parent)
+        self.setWindowIcon(QIcon('protection.png'))
+        self.setWindowTitle("Price Trakcing App")
 
-        # Set fonts
-        self.label_font = font.Font(family="Verdana", size=12, weight="bold")
-        self.entry_font = font.Font(family="Verdana", size=12)
-        self.button_font = font.Font(
-            family="Verdana", size=12, )
-        self.text_font = font.Font(family="Courier New", size=12)
+        self.setWindowFlags(self.windowFlags() & ~
+                            Qt.WindowContextHelpButtonHint)
 
-        # Create and place widgets
-        self.create_widgets()
+        self.setStyleSheet("QDialog {background-color: #7d8780;}")
 
-    def create_widgets(self):
-        # Search Prompt
-        self.search_label = tk.Label(
-            self.root, text="Product:", fg="white", bg="#2E2E2E", font=self.label_font
-        )
-        self.search_label.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
+        self.product_input = QLineEdit(self)
+        self.ok_button = QPushButton("Search", self)
 
-        # Entry for search input
-        self.search_box = tk.Entry(
-            self.root, width=30, bg="#95B2A7", font=self.entry_font)
-        self.search_box.grid(row=0, column=1, padx=10, pady=10, sticky=tk.W)
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.product_input)
+        layout.addWidget(self.ok_button)
 
-        # Search Button
-        self.search_button = tk.Button(
-            self.root, text="Search", command=self.search, bg="#4CAF50", fg="white", width=15, font=self.button_font
-        )
-        self.search_button.grid(
-            row=0, column=2, padx=10, pady=10, sticky=tk.W)
+        self.ok_button.clicked.connect(self.accept)
 
-        # Results Text Widget
-        self.search_info = tk.Text(
-            self.root, height=10, width=60, wrap=tk.WORD, bg="#95B2A7", fg="white", font=self.text_font
-        )
-        self.search_info.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
-
-    def search(self):
-        product = self.search_box.get()
-        print(product)
-        self.search_info.insert(
-            '1.0', "Se cauta pe Emag.....\nSe cauta pe Evomag.....\nSe cauta pe Cel.....")
-        # Schedule the execution of the emag and evomag methods after a delay
-        self.root.after(100, lambda: self.update_search_info(product))
-
-    def update_search_info(self, product):
-        emag_pret = self.emag(product, self.driver)
-        #evomag_pret = self.evomag(product, self.driver)
-        cel_pret = self.cel(product, self.driver)
-        #elefant_pret = self.elefant(product, self.driver)
-        self.search_info.delete('1.0', tk.END)
-        self.search_info.insert('1.0', emag_pret)
-        #self.search_info.insert(tk.END, "\n" + evomag_pret)
-        self.search_info.insert(tk.END, "\n" + cel_pret)
-        #self.search_info.insert(tk.END, "\n" + elefant_pret)
-
-    def emag(self, product):
-        options = Options()
-        options.add_experimental_option("detach", True)
-        driver = webdriver.Chrome(service=Service(
-            ChromeDriverManager().install()), options=options)
-
-        URL = "https://www.emag.ro/search/" + product + "?ref=effective_search"
-
-        driver.get(URL)
-        parent_window = driver.current_window_handle
-        try:
-            check = (driver.find_element(
-                By.XPATH, "//span[@class='title-phrasing title-phrasing-sm text-danger']").text)
-        except NoSuchElementException:
-            check = "All good"
-        if check == "0 rezultate pentru:":
-            nume_emag = "Product dosen't exist"
-            pret_emag = "Price not found"
-        else:
-            # Find and click on the first link
-
-            first_link = driver.find_element(By.XPATH,
-                                             "//body[1]/div[3]/div[2]/div[1]/section[1]/div[1]/div[3]/div[2]/div[6]/div[1]/div[1]/div[1]/div[3]/a[1]").click()
-            try:
-                pret_emag = driver.find_element(
-                    By.XPATH, "//p[@class='product-new-price']/span[@class='product-price'][last()]").text
-                nume_emag = (driver.find_element(
-                    By.XPATH, "/html[@class='doc-desktop ']/body/div[@class='main-container-outer']/div[@class='main-container-inner']/div[@id='main-container']/section[@class='page-section page-section-light'][1]/div[@class='container']/div[@class='page-header d-flex justify-space-between hidden-xs']/h1[@class='page-title']").text)
-            except NoSuchElementException:
-                try:
-                    pret_emag = driver.find_element(
-                        By.XPATH, "//div[@class='product-page-pricing product-highlight']//div//p[@class='product-new-price']").text
-                    nume_emag = (driver.find_element(
-                        By.XPATH, "/html[@class='doc-desktop ']/body/div[@class='main-container-outer']/div[@class='main-container-inner']/div[@id='main-container']/section[@class='page-section page-section-light'][1]/div[@class='container']/div[@class='page-header d-flex justify-space-between hidden-xs']/h1[@class='page-title']").text)
-                except NoSuchElementException:
-                    pret_emag = "Price not found"
-            finally:
-            # Close the current tab
-            driver.close()
-            # Switch back to the main window
-            driver.switch_to.window(self.parent_window)
-
-        return (nume_emag[0:35] + ' PRET: ' + pret_emag)
-
-    def evomag(self, product):
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option('useAutomationExtension', False)
-        driver = webdriver.Chrome(options=options)
-
-        URL = "https://www.evomag.ro/?sn.q=" + product
-        driver.get(URL)
-
-        try:
-            check = (driver.find_element(
-                By.XPATH, "/html/body[@class='homepage']/div[@class='main_wrap']/div[@class='wrap_inside_container main_body_container']/div[@class='meniu_produse_list searchnode']/div[@class='produse_body']/div[@class='produse_liste_filter']/h3/div[@class='sub_taburi_produse sub_taburi_produse-search']/div[@class='noResults']").text)
-        except NoSuchElementException:
-            check = "All good"
-
-        if check == "CRITERIILE DE FILTRARE SELECTATE DE DUMNEAVOASTRA NU AU RETURNAT NICI UN REZULTAT!":
-            nume = "Product dosen't exist"
-            pret = "Price not found"
-        else:
-            first_link = driver.find_element(
-                By.XPATH, "//div[@class='produse_liste_filter']//div[1]//div[1]//div[3]").click()
-            pret = (driver.find_element(
-                By.XPATH, "//div[@class='price_ajax']//div[@class='pret_rons']").text)
-            nume = (driver.find_element(
-                By.XPATH, "//div[@class='product_right_inside slim']").text)
-
-        driver.quit()
-        return (nume[0:35] + ' PRET: ' + pret)
-
-    def cel(self, product):
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option('useAutomationExtension', False)
-        driver = webdriver.Chrome(options=options)
-
-        URL = "https://www.cel.ro/cauta/" + product
-        driver.get(URL)
-
-        try:
-            check = (driver.find_element(
-                By.XPATH, "/html/body[@id='advancedsearchresult']/div[@id='mainWrapper']/div[@class='content-wrapper']/div[@id='bodycode3']/div[@id='bodycode']/div[@class='listingPageWrapper']/div[@class='listingWrapper no-filters']/div[@class='productlisting']/div").text)
-        except NoSuchElementException:
-            check = "All good"
-
-        if check == "Nu sunt produse disponibile":
-            nume = "Product dosen't exist"
-            pret = "Price not found"
-        else:
-            first_link = driver.find_element(
-                By.XPATH, "//body[1]/div[1]/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/h2[1]/a[1]").click()
-            pret = (driver.find_element(
-                By.XPATH, "//span[@id='product-price']").text)
-            nume = (driver.find_element(
-                By.XPATH, "//h1[@id='product-name']").text)
-
-        finally:
-            # Close the current tab
-            driver.close()
-            # Switch back to the main window
-            driver.switch_to.window(self.parent_window)
-            driver.quit()
-        
-        return (nume[0:35] + ' PRET: ' + pret + ' LEI')
-
-    def elefant(self, product):
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option('useAutomationExtension', False)
-        driver = webdriver.Chrome(options=options)
-
-        URL = "https://www.elefant.ro/search?SearchTerm=" + product + "&StockAvailability=true"
-        driver.get(URL)
-
-        try:
-            check = (driver.find_element(
-                By.XPATH, "//div[@class='errorpage-cta']").text)
-            nume = "Product dosen't exist"
-            pret = "Price not found"
-        except NoSuchElementException:
-            check = "All good"
-
-        if check == "NE PARE RĂU, NU EXISTĂ PRODUSE ÎN ACEASTĂ CATEGORIE.":
-            nume = "Product dosen't exist"
-            pret = "Price not found"
-        else:
-            first_link = driver.find_element(
-                By.XPATH, "//div[@class='product-tile js-product-tile js-product-tile-1722011e-b8f5-4f8c-a720-4834d7382cc4']//a[@class='product-title']").click()
-            pret = (driver.find_element(
-                By.XPATH, "//div[@id='product-main-details-price']//div[@class='product-price vendor-offer-data js-vendor-price']").text)
-            nume = (driver.find_element(
-                By.XPATH, "//div[@class='product-title']").text)
-
-        finally:
-            # Close the current tab
-        driver.close()
-            # Switch back to the main window
-        driver.switch_to.window(self.parent_window)
-        driver.quit()
-
-        return (nume[0:35] + ' PRET: ' + pret)
+    def get_product_name(self):
+        return self.product_input.text()
 
 
-app = GUI()
-# Run the application
-app.run()
+class PriceDialog(QDialog):
+    def __init__(self, emag_price, cel_price, parent=None):
+        super(PriceDialog, self).__init__(parent)
+        self.setWindowFlags(self.windowFlags() & ~
+                            QtCore.Qt.WindowContextHelpButtonHint)
+
+        # Set the background color
+        self.setStyleSheet("background-color: #d1eddb;")
+
+        # Set the application icon
+        self.setWindowIcon(QIcon('protection.png'))
+
+        self.setWindowTitle("Price Trakcing App")
+
+        self.emag_price_label = QLabel(f"eMAG Price: {emag_price}", self)
+        self.cel_price_label = QLabel(f"CEL Price: {cel_price}", self)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.emag_price_label)
+        layout.addWidget(self.cel_price_label)
+
+
+# Create PyQt input dialog
+app = QApplication([])
+input_dialog = ProductInputDialog()
+input_result = input_dialog.exec_()
+product_name = input_dialog.get_product_name()
+
+# Initialize variables to store product prices
+emag_price = "Not found"
+cel_price = "Not found"
+
+# After entering the product name
+if input_result == QDialog.Accepted and product_name:
+    # Create a Chrome WebDriver instance
+    browser = webdriver.Chrome()
+    browser.get('https://www.emag.ro/')
+
+    # Find the search input and enter the user-inputted product name
+    search_input_emag = browser.find_element(
+        By.XPATH, '//*[@id="searchboxTrigger"]')
+    search_input_emag.send_keys(product_name)
+
+    # Find the search button and click it
+    search_button_emag = browser.find_element(
+        By.XPATH, '//*[@id="masthead"]/div/div/div[2]/div/form/div[1]/div[2]')
+    search_button_emag.click()
+
+    # Wait for the first product to be present on the page
+    wait_emag = WebDriverWait(browser, 10)
+    try:
+        first_product_emag = wait_emag.until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="card_grid"]/div[1]/div/div/div[3]/a')))
+        first_product_emag.click()
+
+        # Wait for the price element to be present on the page
+        price_element_emag = wait_emag.until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="main-container"]/section[3]/div/div[1]/div[2]/div/div[2]/div[2]/form[1]/div[1]/div[1]/div[1]/div/div/div[1]/p[2]')))
+        # Get the text of the price element
+        emag_price = price_element_emag.text
+    except NoSuchElementException as e:
+        print(f"Error in eMAG: {e}")
+
+    # Close the current tab (emag.ro)
+    # browser.close()
+
+    # Open the CEL tab in the same browser instance
+    browser.get('https://www.cel.ro/')
+
+    # Find the search input and enter the user-inputted product name
+    search_input_cel = browser.find_element(
+        By.XPATH, '//*[@id="keyword"]')
+    search_input_cel.send_keys(product_name)
+
+    # Find the search button and click it
+    search_button_cel = browser.find_element(
+        By.XPATH, '//*[@id="quick_find"]/div[2]/button')
+    search_button_cel.click()
+
+    # Wait for the first product to be present on the page
+    wait_cel = WebDriverWait(browser, 10)
+    try:
+        first_product_cel = wait_cel.until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="bodycode"]/div[2]/div[1]/div[2]/div[1]')))
+        first_product_cel.click()
+
+        # Wait for the price element to be present on the page
+        price_element_cel = wait_cel.until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="product-price"]')))
+        # Get the text of the price element
+        cel_price = price_element_cel.text
+    except NoSuchElementException:
+        pass
+
+    # Close the current tab (cel.ro)
+    browser.close()
+
+# Display the product prices using PyQt dialog
+price_dialog = PriceDialog(emag_price, cel_price)
+price_dialog.exec_()
